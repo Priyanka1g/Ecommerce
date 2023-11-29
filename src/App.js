e0fad8ab4a2f2aa5e8031c38dd44dc1ea6391d46
Priyanka Gupta <Priyanka.Gupta@q3infotech.net>
2023-11-29 15:28:04 +0530

    format code

--- src/App.js
+++ src/App.js
@@ -21,77 +21,72 @@ const usersData = require('./users.json');
 const usersArray = usersData.users; // Access the "users" array
 
 function App() {
-  const dispatch = useDispatch();
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  const signupInfo = useSelector(selectSignupInfo);
+    const dispatch = useDispatch();
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+    const signupInfo = useSelector(selectSignupInfo);
 
-  const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
 
-  const signupHandler = (name, email, password) => {
-    // You would typically hash the password before storing it in a real-world scenario
-    const newUser = { name, email, password };
+    const signupHandler = (name, email, password) => {
+        const newUser = { name, email, password };
 
-    // Push the new user to the array
-    usersArray.push(newUser);
+        // Push the new user to the array
+        usersArray.push(newUser);
+        dispatch(signup(newUser));
 
-    // Assuming that you have a `signup` action that updates the Redux store with the user information
-    dispatch(signup(newUser));
+        // Automatically log in after signup
+        dispatch(login());
 
-    // Automatically log in after signup
-    dispatch(login());
+        // Display a success message
+        toast.success('Signup successful!');
+    };
 
-    // Display a success message
-    toast.success('Signup successful!');
-  };
+    const loginHandler = (email, password) => {
+        // Use find on the array
+        const user = usersArray.find(u => u.email === email && u.password === password);
 
-  const loginHandler = (email, password) => {
-    // Use find on the array
-    const user = usersArray.find(u => u.email === email && u.password === password);
+        if (user) {
+            dispatch(login());
+            toast.success('Login successful!');
+        } else {
+            toast.error('Invalid login credentials. Please try again.');
+        }
+    };
 
-    if (user) {
-      dispatch(login());
-      toast.success('Login successful!');
-    } else {
-      toast.error('Invalid login credentials. Please try again.');
-    }
-  };
+    const logoutHandler = () => {
+        dispatch(logout());
+        toast.success('Logout successful!');
+    };
 
-  const logoutHandler = () => {
-    dispatch(logout());
-    toast.success('Logout successful!');
-  };
+    useEffect(() => {
+        if (signupInfo.userEmail) {
+            dispatch(login());
+        }
+    }, [dispatch, signupInfo]);
 
-  useEffect(() => {
-    // No need to check localStorage; you can use a server-based solution here
-    // For simplicity, let's assume a user is logged in if the signupInfo is available
-    if (signupInfo.userEmail) {
-      dispatch(login());
-    }
-  }, [dispatch, signupInfo]);
-
-  return (
-    <>
-      {isLoggedIn ? (
+    return (
         <>
-          <Navbar onLogout={logoutHandler} getDataHandler={setSearchTerm} />
-          <Routes>
-            <Route path='/' element={<ProductCards searchTerm={searchTerm} />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='/checkout' element={<Checkout />} />
-            <Route path='/details/:id' element={<ProductDetails />} />
-            <Route path='/userprofile' element={<UserProfile />} />
-            <Route path='/orderpage' element={<OrderItems />} />
-          </Routes>
-          <Toaster />
+            {isLoggedIn ? (
+                <>
+                    <Navbar onLogout={logoutHandler} getDataHandler={setSearchTerm} />
+                    <Routes>
+                        <Route path='/' element={<ProductCards searchTerm={searchTerm} />} />
+                        <Route path='/cart' element={<Cart />} />
+                        <Route path='/checkout' element={<Checkout />} />
+                        <Route path='/details/:id' element={<ProductDetails />} />
+                        <Route path='/userprofile' element={<UserProfile />} />
+                        <Route path='/orderpage' element={<OrderItems />} />
+                    </Routes>
+                    <Toaster />
+                </>
+            ) : (
+                <Routes>
+                    <Route path="/" element={<Login onLogin={loginHandler} />} />
+                    <Route path="/signup" element={<Signup onSignup={signupHandler} />} />
+                </Routes>
+            )}
         </>
-      ) : (
-        <Routes>
-          <Route path="/" element={<Login onLogin={loginHandler} />} />
-          <Route path="/signup" element={<Signup onSignup={signupHandler} />} />
-        </Routes>
-      )}
-    </>
-  );
+    );
 }
 
 export default App;
--- src/components/userService.js
+++
@@ -1,8 +0,0 @@
-// userService.js
-import usersData from '../users.json';
-
-export const users = usersData.users; // Extract the "users" array
-
-export const getUserByEmail = (email) => {
-  return users.find(user => user.email === email);
-};
